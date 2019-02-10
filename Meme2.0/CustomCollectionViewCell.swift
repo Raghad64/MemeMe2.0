9d986b7a2c9f50b5fd05579dc7e84293b3573278
Raghad Abdulghani <39538202+Raghad64@users.noreply.github.com>
2019-02-10 12:53:21 +0300

    Added CustomCollectionViewCell.swift file

---
+++ Meme2.0/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  MemeMe2.0
+//
+//  Created by Raghad Mah on 08/12/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
